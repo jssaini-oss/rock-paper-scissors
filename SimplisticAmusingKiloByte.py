afc07b5cbbb43600de0d5c11c4b938bff4020519
jssaini-oss <jssaini@alumni.iu.edu>
2025-09-21 08:03:19 -0400

    Create SimplisticAmusingKiloByte

---
+++ SimplisticAmusingKiloByte.py
@@ -0,0 +1,11 @@
+def greeting():
+  return = "Hi, want to play a game of rock paper scissors?"
+
+response = greeting ()
+print(response)
+
+def get_choices():
+  player_choice = "rock"
+  computer_choice = "paper"
+
+  return computer_choice
